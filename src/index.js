fc11d3f7f754bd152ec5fd1934ad38e7f4c6d711
Vadim Bakunovich <vadim.bakunovich@gmail.com>
2021-08-02 21:49:54 +0300

    feat: add function code to index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (!arr || !arr.length) return 0;
+  else return arr.sort()[0];
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (!arr || !arr.length) return 0;
+  else return arr.sort().reverse()[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!arr || !arr.length) return 0;
+  else return arr.reduce((sum, i) => sum + i) / arr.length;
 }
